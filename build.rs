92953548d3ee86b192da93cd495210047fd17d43
ramdanhere <ramdanhere04@gmail.com>
2022-01-27 10:36:34 +0000

    dev

--- build.rs
+++ build.rs
@@ -3,7 +3,12 @@ extern crate cmake;
 use cmake::Config;
 
 fn main() {
-    let dst = Config::new("solidity").build();
+    let dst = Config::new("solidity")
+        .define("TESTS", "OFF")
+        .define("TOOLS", "OFF")
+        .define("USE_Z3", "OFF")
+        .define("USE_CVC4", "OFF")
+        .build();
 
     println!("cargo:rust-link-search=native={}/lib", dst.display());
 }
